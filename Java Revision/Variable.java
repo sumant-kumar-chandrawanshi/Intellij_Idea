a8bfbd118ea87fda5ebd590a4e30c261b0ca6f92
sumant kumar chandrawasnhi <sumantkumarchandrawanshi@gamil.com>
2026-01-06 11:02:41 +0530

    "Feat: Add New File That Name is Variable"

---
+++ Java Revision/Variable.java
@@ -0,0 +1,10 @@
+public class Variable {
+    public static void main(String[] args) {
+//       Variable: - Variable is a container that value.
+        int a=10;
+        System.out.println(a);
+//        Output: 10
+d
+
+    }
+}
