22a40803c43ef62449d1c7ecfd4856fb7cd358e2
sumant kumar chandrawasnhi <sumantkumarchandrawanshi@gamil.com>
2026-01-06 11:08:20 +0530

    "Feat: Add New File That Name is Function"

---
+++ Java Revision/Function.java
@@ -0,0 +1,10 @@
+public class Function {
+    public void hello(){
+        System.out.println("hello");
+    }
+    public static void main(String[] args) {
+        Function obj=new Function();
+        obj.hello();
+    }
+
+}
