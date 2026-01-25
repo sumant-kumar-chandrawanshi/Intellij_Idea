dde77d2ce9ae667f4908bd6f4ee1550ffc219b73
NitishKumar82 <nitishranjan135@gmail.com>
2026-01-25 14:32:53 +0530

    "Feat: Add New File That Name is Function"

---
+++ Java Revision/Second.java
@@ -0,0 +1,33 @@
+public class Second{
+    public static void main(String[] args) {
+        System.out.println("hello World");
+        System.out.println("have a good day!");
+        System.out.println("Learning Java is fun");
+        System.out.println(2+4);
+        System.out.println(4*3);
+        System.out.println(20/2);
+        //This is a comment
+        /*
+        this is multi line comment in java
+         */
+//       Variable in java
+
+        Stirng name="Sumant Kumar";
+        System.out.println(name);
+
+        int myNum=15;
+        System.out.println(myNum);
+
+        int num;
+        num=30;
+        System.out.println(num);
+
+        num=40;
+        System.out.println(num);
+
+
+
+
+
+    }
+}
---
+++ Java Revision/class_file.java
@@ -0,0 +1,7 @@
+public class class_file {
+    public static void main(String[] args) {
+        System.out.println("Use of class");
+
+
+    }
+}
