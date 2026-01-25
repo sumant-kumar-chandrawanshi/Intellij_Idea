d8f9e0e5f3965fb77a3d26873d4f02c3dd5b61b9
NitishKumar82 <nitishranjan135@gmail.com>
2026-01-25 14:29:58 +0530

    "Feat: Add New File That Name is Function"

--- Java Revision/Function.java
+++ Java Revision/Function.java
@@ -2,9 +2,23 @@ public class Function {
     public void hello(){
         System.out.println("hello");
     }
+    public void add(int a, int b){
+        System.out.println(a+b);
+    }
+    public void sub(int a, int b){
+        System.out.println(a-b);
+    }
+
+    public void prod(int a,int b){
+        System.out.println(a*b);
+    }
+
+
     public static void main(String[] args) {
         Function obj=new Function();
         obj.hello();
+        obj.add(10,30);
+        obj.sub(30,10);
     }
 
 }
